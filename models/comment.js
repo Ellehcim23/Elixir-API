f6489f3c5e016a7fc8f933b868bee739e33f7711
ellehcim burton <ellehcimburton@ellehcims-MBP.lan>
2023-07-09 18:24:13 -0400

    feat: created models

---
+++ models/comment.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    title: String,
+    body: {type: String, required: true},
+    createdBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+},{ timestamps:true })
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
---
+++ models/user.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    fullName: {type: String, required: true},
+    birthdate: {type: Date, required: true},
+    location: String,
+    recipesByUser: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
+    commentsByUser: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+    following: Array,
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
+    avatar: String
+},{ timestamps:true })
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
